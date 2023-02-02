7b7f6e709ec97875d7c82f9596ee24641f2fa213
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-02-02 10:42:51 -0800

    Update README

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,23 @@
-//! Rust library for tokenizing text for GPT using tiktoken
-//!
-//! ## Counting token length
-//!
-//!```
-//!  use tiktoken_rs::tiktoken::p50k_base;
-//!
-//!  let bpe = p50k_base().unwrap();
-//!  let tokens = bpe.encode_with_special_tokens("This is a test         with a lot of spaces");
-//!  println!("Token count: {}", tokens.len());
-//!```
-//!
-//! ## Examples
-//! For full working examples for all supported features see [examples](https://github.com/zurawiki/tiktoken-rs/tree/main/examples) directory in the repository.
-//!
+/*!
+
+Rust library for tokenizing text for GPT using tiktoken
+
+## Counting token length
+
+```
+use tiktoken_rs::tiktoken::p50k_base;
+
+let bpe = p50k_base().unwrap();
+let tokens = bpe.encode_with_special_tokens(
+  "This is a sentence   with spaces"
+);
+println!("Token count: {}", tokens.len());
+```
+
+## Examples
+For full working examples for all supported features see [examples](https://github.com/zurawiki/tiktoken-rs/tree/main/examples) directory in the repository.
+
+*/
+
 /// Adaptation of the tiktoken crate for use in Rust projects
 pub mod tiktoken;
