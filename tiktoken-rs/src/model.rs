b0c2cc1ec381a6f0376d043de43ad002d31b2142
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-03-06 23:25:20 -0500

    chore: GitHub actions checkout submodules

---
+++ tiktoken-rs/src/model.rs
@@ -0,0 +1,85 @@
+const MODEL_PREFIX_TO_ENCODING: &[(&str, &str)] = &[
+    // chat
+    ("gpt-3.5-turbo-", "cl100k_base"), // e.g, gpt-3.5-turbo-0301, -0401, etc.
+];
+
+const MODEL_TO_ENCODING: &[(&str, &str)] = &[
+    // chat
+    ("gpt-3.5-turbo", "cl100k_base"),
+    // text
+    ("text-davinci-003", "p50k_base"),
+    ("text-davinci-002", "p50k_base"),
+    ("text-davinci-001", "r50k_base"),
+    ("text-curie-001", "r50k_base"),
+    ("text-babbage-001", "r50k_base"),
+    ("text-ada-001", "r50k_base"),
+    ("davinci", "r50k_base"),
+    ("curie", "r50k_base"),
+    ("babbage", "r50k_base"),
+    ("ada", "r50k_base"),
+    // code
+    ("code-davinci-002", "p50k_base"),
+    ("code-davinci-001", "p50k_base"),
+    ("code-cushman-002", "p50k_base"),
+    ("code-cushman-001", "p50k_base"),
+    ("davinci-codex", "p50k_base"),
+    ("cushman-codex", "p50k_base"),
+    // edit
+    ("text-davinci-edit-001", "p50k_edit"),
+    ("code-davinci-edit-001", "p50k_edit"),
+    // embeddings
+    ("text-embedding-ada-002", "cl100k_base"),
+    // old embeddings
+    ("text-similarity-davinci-001", "r50k_base"),
+    ("text-similarity-curie-001", "r50k_base"),
+    ("text-similarity-babbage-001", "r50k_base"),
+    ("text-similarity-ada-001", "r50k_base"),
+    ("text-search-davinci-doc-001", "r50k_base"),
+    ("text-search-curie-doc-001", "r50k_base"),
+    ("text-search-babbage-doc-001", "r50k_base"),
+    ("text-search-ada-doc-001", "r50k_base"),
+    ("code-search-babbage-code-001", "r50k_base"),
+    ("code-search-ada-code-001", "r50k_base"),
+    // open source
+    ("gpt2", "gpt2"),
+];
+
+/// Returns the encoding used by a model.
+///
+/// TODO use hashmap
+pub fn encoding_for_model(model_name: &str) -> Option<&str> {
+    if let Some(encoding) = MODEL_TO_ENCODING
+        .iter()
+        .find(|(model, _)| *model == model_name)
+    {
+        return Some(encoding.1);
+    }
+    if let Some(encoding) = MODEL_PREFIX_TO_ENCODING
+        .iter()
+        .find(|(model_prefix, _)| model_name.starts_with(*model_prefix))
+    {
+        return Some(encoding.1);
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encoding_for_model() {
+        assert_eq!(encoding_for_model("gpt-3.5-turbo"), Some("cl100k_base"));
+        assert_eq!(
+            encoding_for_model("gpt-3.5-turbo-0301"),
+            Some("cl100k_base")
+        );
+        assert_eq!(encoding_for_model("text-davinci-003"), Some("p50k_base"));
+        assert_eq!(
+            encoding_for_model("code-search-ada-code-001"),
+            Some("r50k_base")
+        );
+        assert_eq!(encoding_for_model("foo"), None);
+    }
+}
---
+++ tiktoken-rs/src/singleton.rs
@@ -0,0 +1,52 @@
+use std::sync::Arc;
+
+use lazy_static::lazy_static;
+use parking_lot::Mutex;
+
+use tiktoken::CoreBPE;
+
+use crate::{cl100k_base, p50k_base, p50k_edit, r50k_base};
+
+/// Returns a singleton instance of the r50k_base tokenizer. (also known as `gpt2`)
+/// Use for GPT-3 models like `davinci`
+///
+/// This function will only initialize the tokenizer once, and then return a reference the tokenizer
+pub fn r50k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+    lazy_static! {
+        static ref R50K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(r50k_base().unwrap()));
+    }
+    R50K_BASE.clone()
+}
+
+/// Returns a singleton instance of the p50k_base tokenizer.
+/// Use for Code models, `text-davinci-002`, `text-davinci-003`
+///
+/// This function will only initialize the tokenizer once, and then return a reference the tokenizer.
+pub fn p50k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+    lazy_static! {
+        static ref P50K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(p50k_base().unwrap()));
+    }
+    P50K_BASE.clone()
+}
+
+/// Returns a singleton instance of the p50k_edit tokenizer.
+/// Use for edit models like `text-davinci-edit-001`, `code-davinci-edit-001`
+///
+/// This function will only initialize the tokenizer once, and then return a reference the tokenizer.
+pub fn p50k_edit_singleton() -> Arc<Mutex<CoreBPE>> {
+    lazy_static! {
+        static ref P50K_EDIT: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(p50k_edit().unwrap()));
+    }
+    P50K_EDIT.clone()
+}
+
+/// Returns a singleton instance of the cl100k_base tokenizer.
+/// Use for ChatGPT models, `text-embedding-ada-002`
+///
+/// This function will only initialize the tokenizer once, and then return a reference the tokenizer
+pub fn cl100k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+    lazy_static! {
+        static ref CL100K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(cl100k_base().unwrap()));
+    }
+    CL100K_BASE.clone()
+}
