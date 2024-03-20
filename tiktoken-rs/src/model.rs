a88091f9d926bafbed4b6284c4d348cabe360281
Mikael Souza <mikael.souza.cc@gmail.com>
2024-03-20 13:27:42 -0400

    Add `gpt-3.5-turbo-0125` (#60)
    * add `gpt-3.5-turbo-0125`
    
    * fix formatting issues

--- tiktoken-rs/src/model.rs
+++ tiktoken-rs/src/model.rs
@@ -44,6 +44,9 @@ pub fn get_context_size(model: &str) -> usize {
     if starts_with_any!(model, "gpt-4") {
         return 8192;
     }
+    if starts_with_any!(model, "gpt-3.5-turbo-0125") {
+        return 16_385;
+    }
     if starts_with_any!(model, "gpt-3.5-turbo-1106") {
         return 16_385;
     }
