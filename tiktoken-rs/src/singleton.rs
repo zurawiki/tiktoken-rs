048f3c3e4f88a1735717faf9c517ea11325b735e
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-05-08 14:33:13 +0200

    Remove need for Arc<Mutex<>> on singletons
    Since there aren't mutable methods on CoreBPE, you can remove the
    indirections and locks and just return a static reference.

--- tiktoken-rs/src/singleton.rs
+++ tiktoken-rs/src/singleton.rs
@@ -1,7 +1,4 @@
-use std::sync::Arc;
-
 use lazy_static::lazy_static;
-use parking_lot::Mutex;
 
 use crate::vendor_tiktoken::CoreBPE;
 
@@ -11,53 +8,53 @@ use crate::{cl100k_base, o200k_base, p50k_base, p50k_edit, r50k_base};
 /// Use for GPT-3 models like `davinci`
 ///
 /// This function will only initialize the tokenizer once, and then return a reference the tokenizer
-pub fn r50k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+pub fn r50k_base_singleton() -> &'static CoreBPE {
     lazy_static! {
-        static ref R50K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(r50k_base().unwrap()));
+        static ref R50K_BASE: CoreBPE = r50k_base().unwrap();
     }
-    R50K_BASE.clone()
+    &R50K_BASE
 }
 
 /// Returns a singleton instance of the p50k_base tokenizer.
 /// Use for Code models, `text-davinci-002`, `text-davinci-003`
 ///
 /// This function will only initialize the tokenizer once, and then return a reference the tokenizer.
-pub fn p50k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+pub fn p50k_base_singleton() -> &'static CoreBPE {
     lazy_static! {
-        static ref P50K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(p50k_base().unwrap()));
+        static ref P50K_BASE: CoreBPE = p50k_base().unwrap();
     }
-    P50K_BASE.clone()
+    &P50K_BASE
 }
 
 /// Returns a singleton instance of the p50k_edit tokenizer.
 /// Use for edit models like `text-davinci-edit-001`, `code-davinci-edit-001`
 ///
 /// This function will only initialize the tokenizer once, and then return a reference the tokenizer.
-pub fn p50k_edit_singleton() -> Arc<Mutex<CoreBPE>> {
+pub fn p50k_edit_singleton() -> &'static CoreBPE {
     lazy_static! {
-        static ref P50K_EDIT: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(p50k_edit().unwrap()));
+        static ref P50K_EDIT: CoreBPE = p50k_edit().unwrap();
     }
-    P50K_EDIT.clone()
+    &P50K_EDIT
 }
 
 /// Returns a singleton instance of the cl100k_base tokenizer.
 /// Use for ChatGPT models, `text-embedding-ada-002`
 ///
 /// This function will only initialize the tokenizer once, and then return a reference the tokenizer
-pub fn cl100k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+pub fn cl100k_base_singleton() -> &'static CoreBPE {
     lazy_static! {
-        static ref CL100K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(cl100k_base().unwrap()));
+        static ref CL100K_BASE: CoreBPE = cl100k_base().unwrap();
     }
-    CL100K_BASE.clone()
+    &CL100K_BASE
 }
 
 /// Returns a singleton instance of the o200k_base tokenizer.
 /// Use for GPT-4o models.
 ///
 /// This function will only initialize the tokenizer once, and then return a reference the tokenizer
-pub fn o200k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+pub fn o200k_base_singleton() -> &'static CoreBPE {
     lazy_static! {
-        static ref O200K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(o200k_base().unwrap()));
+        static ref O200K_BASE: CoreBPE = o200k_base().unwrap();
     }
-    O200K_BASE.clone()
+    &O200K_BASE
 }
